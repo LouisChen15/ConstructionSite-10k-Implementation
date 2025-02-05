c52cbd457185d167095cc05f564559515262b5b2
LouisChen15 <97120485+LouisChen15@users.noreply.github.com>
2025-02-05 10:45:48 -0800

    Add files via upload

---
+++ description_query_llava.py
@@ -0,0 +1,99 @@
+import json
+import os
+from predict import Predictor
+
+prompts = {
+    "prompt1": "Please describe the image. Your description should include is there any people, construction equipment, or material stockpiles in the image, where are they approximately located in the image, and how many are they. Please describe the activities. Please also include any outstanding construction elements in the image.", 
+    "prompt2": "Please describe the image. Try your best to find the number, location, and activities of people, and construction equipment. Also, find the type and number of material stockpiles in the image. Also describe anything unusual in the image.",
+    "prompt3": "Please describe the image. Try your best to find the number, location, and activities of people, and construction equipment. And the type and number of material stockpiles in the image. Also describe anything unusual in the image. Please describe with only one paragraph.",
+    "prompt4": "Please describe the image. Your description should focus on the number, location, and activities of people, construction equipment, or material stockpiles in the image. Please describe with only one paragraph.",
+    "prompt5": "Please describe the image. Your description should include the number, location, and activities of people, construction equipment, or material stockpiles in the image. Do not make assumptions and be concise. Please describe with only one paragraph."
+}
+
+
+def main():
+
+    # Replace 'your_script.py' with the actual name of the Python script you want to execute
+    image_folder = "/home/xuezheng/Desktop/VLM/10k_images"
+    data_split_id = "random1"
+
+    with open("/home/xuezheng/Desktop/VLM/Annotations/dataset10k_metadata.json", "r") as file:
+        data_split = json.load(file)
+    test_split = data_split['test_split']
+
+    image_list = test_split
+    image_list = sorted(image_list)
+    tot = len(image_list)
+
+    predictor = Predictor()
+
+    # for prompt_id, user_input in prompts.items():
+    for seed_num in range(5):
+        seed_num += 1
+        seed_str = str(seed_num)
+        
+        storage_file = "llava_v15_13B_" + data_split_id + "_" + "prompt5" + "_" + "seed" + seed_str + ".json"
+        print(storage_file)
+
+        try:
+            with open(storage_file, 'r') as file:
+                data = json.load(file)
+                finished_list = list(data.keys())
+        except:
+            finished_list = []
+        
+        image_list_to_finish = [image_id for image_id in image_list if image_id not in finished_list]
+
+        instance_id_list = []
+        reply_list = []
+        i = 1+len(finished_list)
+
+        # Loop through all examples
+        for image in image_list_to_finish:
+            print(f"Running {i}/{tot}")
+            print(image)
+            i += 1
+
+            image_path = os.path.join(image_folder, image + '.jpg')
+            instance_id = image
+            output = predictor.predict(image=image_path, prompt=prompts["prompt5"])
+            
+            cleared_reply = ''
+            for word in output:
+                cleared_reply += word
+            cleared_reply = cleared_reply.replace('\n\n', '')
+
+            instance_id_list.append(instance_id)
+            reply_list.append(cleared_reply)
+
+            # intermediate save
+            if len(instance_id_list) == 1:
+                answer = dict(zip(instance_id_list, reply_list))
+                print(answer)
+                instance_id_list = []
+                reply_list = []
+                if not os.path.exists(storage_file):
+                    with open(storage_file, 'w') as file: 
+                        json.dump(answer, file)
+                else:
+                    with open(storage_file, 'r') as file: 
+                        data = json.load(file)
+                        answer.update(data)
+                    with open(storage_file, "w") as file:
+                        json.dump(answer, file)
+
+        # Write and sort the result again outside the loop
+        answer = dict(zip(instance_id_list, reply_list)) # this is possibly an empty dictionary
+
+        with open(storage_file, 'r') as file: 
+            data = json.load(file)
+            answer.update(data)
+
+        sorted_dict = dict(sorted(answer.items(), key=lambda item: int(item[0])))  
+
+        with open(storage_file, "w") as file:
+            json.dump(sorted_dict, file)
+
+
+if __name__ == "__main__":
+    main()
---
+++ predict.py
@@ -0,0 +1,102 @@
+import torch
+
+from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN
+from llava.conversation import conv_templates, SeparatorStyle
+from llava.model.builder import load_pretrained_model
+from llava.utils import disable_torch_init
+from llava.mm_utils import tokenizer_image_token
+from transformers.generation.streamers import TextIteratorStreamer
+
+from PIL import Image
+
+import requests
+from io import BytesIO
+
+from cog import BasePredictor, Input, Path, ConcatenateIterator
+import time
+import subprocess
+from threading import Thread
+
+import os
+
+
+class Predictor(BasePredictor):
+
+    def __init__(self):
+        super().__init__()  # Call the constructor of the base class if needed
+        self.setup()  # Call the setup method upon object creation
+
+    def setup(self):
+        """Load the model into memory to make running multiple predictions efficient"""
+        disable_torch_init()
+        self.tokenizer, self.model, self.image_processor, self.context_len = load_pretrained_model("liuhaotian/llava-v1.5-13b", model_name="llava-v1.5-13b", model_base=None, load_8bit=False, load_4bit=True)
+
+    def predict(
+        self,
+        image,
+        prompt,
+        top_p=1.0,
+        temperature=0.0,
+        max_tokens=1024,
+    ) -> ConcatenateIterator[str]:
+        """Run a single prediction on the model"""
+    
+        conv_mode = "llava_v1"
+        conv = conv_templates[conv_mode].copy()
+    
+        image_data = load_image(str(image))
+        image_tensor = self.image_processor.preprocess(image_data, return_tensors='pt')['pixel_values'].half().cuda()
+    
+        # loop start
+    
+        # just one turn, always prepend image token
+        inp = DEFAULT_IMAGE_TOKEN + '\n' + prompt
+        conv.append_message(conv.roles[0], inp)
+
+        conv.append_message(conv.roles[1], None)
+        prompt = conv.get_prompt()
+    
+        input_ids = tokenizer_image_token(prompt, self.tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).cuda()
+        stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
+        keywords = [stop_str]
+        streamer = TextIteratorStreamer(self.tokenizer, skip_prompt=True, timeout=120.0)
+    
+        with torch.inference_mode():
+            thread = Thread(target=self.model.generate, kwargs=dict(
+                inputs=input_ids,
+                images=image_tensor,
+                do_sample=True,
+                temperature=temperature,
+                top_p=top_p,
+                max_new_tokens=max_tokens,
+                streamer=streamer,
+                use_cache=True))
+            thread.start()
+            # workaround: second-to-last token is always " "
+            # but we want to keep it if it's not the second-to-last token
+            prepend_space = False
+            for new_text in streamer:
+                if new_text == " ":
+                    prepend_space = True
+                    continue
+                if new_text.endswith(stop_str):
+                    new_text = new_text[:-len(stop_str)].strip()
+                    prepend_space = False
+                elif prepend_space:
+                    new_text = " " + new_text
+                    prepend_space = False
+                if len(new_text):
+                    yield new_text
+            if prepend_space:
+                yield " "
+            thread.join()
+    
+
+def load_image(image_file):
+    if image_file.startswith('http') or image_file.startswith('https'):
+        response = requests.get(image_file)
+        image = Image.open(BytesIO(response.content)).convert('RGB')
+    else:
+        image = Image.open(image_file).convert('RGB')
+    return image
+
